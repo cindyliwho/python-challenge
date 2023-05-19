b5f627ccd6e06070d4107b83831fcb4a70596ac4
lipeiyi <lipeiyi@students.cs.ubc.ca>
2023-05-18 18:08:11 -0700

    finished all

---
+++ PyPoll/main.py
@@ -0,0 +1,45 @@
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+
+    csv_header = next(csvreader)
+    #print(f"csv header: {csv_header}")
+
+    totalVotes = 0
+    voteDict = {}
+    for row in csvreader: 
+        totalVotes = totalVotes + 1
+        if row[2] not in voteDict.keys(): 
+            voteDict[row[2]] = 1
+
+        else: 
+            voteDict[row[2]] = voteDict.get(row[2]) + 1
+    #print(voteDict)
+
+    percentageDict= {}
+    maxVote = 0
+    winner = ""
+    for candidate in voteDict:
+        percentage = (voteDict[candidate]/totalVotes)*100
+        percentageDict[candidate] = percentage
+        if voteDict[candidate] > maxVote:
+            maxVote = voteDict[candidate]
+            winner = candidate
+    #print(percentageDict)
+    #print(winner)
+    #print(voteDict[candidate])
+    
+    outputStr1 = list(voteDict)[0] + ": " + "%.3f" % percentageDict.get(list(voteDict)[0]) + "% (" + str(voteDict.get(list(voteDict)[0])) + ")"
+    outputStr2 = list(voteDict)[1] + ": " + "%.3f" % percentageDict.get(list(voteDict)[1]) + "% (" + str(voteDict.get(list(voteDict)[1])) + ")"
+    outputStr3 = list(voteDict)[2] + ": " + "%.3f" % percentageDict.get(list(voteDict)[2]) + "% (" + str(voteDict.get(list(voteDict)[2])) + ")"
+
+
+with open('analysis/summary.txt', 'w') as f:
+     f.write(f"Election Results \n ------------------------- \n Total Votes: {totalVotes} \n ------------------------- \n {outputStr1} \n {outputStr2} \n {outputStr3} \n ------------------------- \n Winner: {winner} \n -------------------------")
+
+print(f"Election Results \n ------------------------- \n Total Votes: {totalVotes} \n ------------------------- \n {outputStr1} \n {outputStr2} \n {outputStr3} \n ------------------------- \n Winner: {winner} \n -------------------------")
+
